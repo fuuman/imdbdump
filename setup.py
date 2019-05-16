ece91e4a0303b9feece874ef7a03bfb9ab1087e0
Marco Schanz <mschanz@uni-koblenz.de>
2019-05-16 22:35:46 +0200

    update setup.py

--- setup.py
+++ setup.py
@@ -8,4 +8,5 @@ setup(name='imdbdump',
       author_email='mschanz1210@gmail.com',
       license='GNU GPLv3',
       packages=['imdbdump'],
+      install_requires=['requests-html'],
       zip_safe=False)
