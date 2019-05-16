6e81a284b5e63004a8c02ac62225c85f20d9a7ec
Marco Schanz <mschanz@uni-koblenz.de>
2019-05-16 22:37:30 +0200

    update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='imdbdump',
-      version='0.1.0',
+      version='0.1.1',
       description='Helps you to dump TV show IMDb ratings in machine-readable formats.',
       url='http://github.com/fuuman/imdbdump',
       author='Marco Schanz',
