ce3593382ecfffef35aa86ab9ffc5e6e5e8eaeb0
Marco Schanz <mschanz@uni-koblenz.de>
2019-05-17 00:14:01 +0200

    add long_description

--- setup.py
+++ setup.py
@@ -1,11 +1,19 @@
 from setuptools import setup
 
+# read the contents of your README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(name='imdbdump',
       version='0.1.1',
       description='Helps you to dump TV show IMDb ratings in machine-readable formats.',
       url='http://github.com/fuuman/imdbdump',
       author='Marco Schanz',
       author_email='mschanz1210@gmail.com',
+      long_description=long_description,
+      long_description_content_type='text/markdown'
       license='GNU GPLv3',
       packages=['imdbdump'],
       install_requires=['requests-html'],
