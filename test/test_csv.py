6761664af941a07533209c566d46ec3bc0b0f0c2
Marco Schanz <mschanz@uni-koblenz.de>
2019-05-17 11:57:44 +0200

    change tests to sopranos

--- test/test_csv.py
+++ test/test_csv.py
@@ -4,13 +4,13 @@ import csv
 from imdbdump.imdb_fetcher import ImdbFetcher
 
 def test_json():
-    imdb = ImdbFetcher('tt0303461')
+    imdb = ImdbFetcher('tt0141842')
     imdb.save_csv()
-    with open('test/expected_firefly:_der_aufbruch_der_serenity.csv', 'r') as expected_csv_file:
+    with open('test/expected_die_sopranos.csv', 'r') as expected_csv_file:
         reader = csv.reader(expected_csv_file)
         expected_csv = list(map(tuple, reader))
-    with open('output/firefly:_der_aufbruch_der_serenity.csv', 'r') as csv_file:
+    with open('output/die_sopranos.csv', 'r') as csv_file:
         reader = csv.reader(csv_file)
         my_csv = list(map(tuple, reader))
     assert expected_csv == my_csv
-    os.remove('output/firefly:_der_aufbruch_der_serenity.csv')
+    os.remove('output/die_sopranos.csv')
--- test/test_json.py
+++ test/test_json.py
@@ -4,13 +4,13 @@ import os
 from imdbdump.imdb_fetcher import ImdbFetcher
 
 def test_json():
-    imdb = ImdbFetcher('tt0303461')
+    imdb = ImdbFetcher('tt0141842')
     imdb.save_json()
-    with open('test/expected_firefly:_der_aufbruch_der_serenity.json', 'r') as expected_js_file:
+    with open('test/expected_die_sopranos.json', 'r') as expected_js_file:
         expected_js = json.load(expected_js_file)
-    with open('output/firefly:_der_aufbruch_der_serenity.json', 'r') as js_file:
+    with open('output/die_sopranos.json', 'r') as js_file:
         my_js = json.load(js_file)
     assert expected_js == my_js
-    os.remove('output/firefly:_der_aufbruch_der_serenity.json')
+    os.remove('output/die_sopranos.json')
 
 
