fc0b00d50cb6d596fffedd6775009d6bbe553b8d
Marco Schanz <mschanz@uni-koblenz.de>
2019-05-17 12:14:17 +0200

    fix tests

--- test/test_csv.py
+++ test/test_csv.py
@@ -6,11 +6,11 @@ from imdbdump.imdb_fetcher import ImdbFetcher
 def test_json():
     imdb = ImdbFetcher('tt0141842')
     imdb.save_csv()
-    with open('test/expected_die_sopranos.csv', 'r') as expected_csv_file:
+    with open('test/expected_the_sopranos.csv', 'r') as expected_csv_file:
         reader = csv.reader(expected_csv_file)
         expected_csv = list(map(tuple, reader))
-    with open('output/die_sopranos.csv', 'r') as csv_file:
+    with open('output/the_sopranos.csv', 'r') as csv_file:
         reader = csv.reader(csv_file)
         my_csv = list(map(tuple, reader))
     assert expected_csv == my_csv
-    os.remove('output/die_sopranos.csv')
+    os.remove('output/the_sopranos.csv')
--- test/test_json.py
+++ test/test_json.py
@@ -6,11 +6,11 @@ from imdbdump.imdb_fetcher import ImdbFetcher
 def test_json():
     imdb = ImdbFetcher('tt0141842')
     imdb.save_json()
-    with open('test/expected_die_sopranos.json', 'r') as expected_js_file:
+    with open('test/expected_the_sopranos.json', 'r') as expected_js_file:
         expected_js = json.load(expected_js_file)
-    with open('output/die_sopranos.json', 'r') as js_file:
+    with open('output/the_sopranos.json', 'r') as js_file:
         my_js = json.load(js_file)
     assert expected_js == my_js
-    os.remove('output/die_sopranos.json')
+    os.remove('output/the_sopranos.json')
 
 
